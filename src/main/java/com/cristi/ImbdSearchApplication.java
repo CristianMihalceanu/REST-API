eedbd601a15a43324818ff809001257ca3cc2170
CristianMihalceanu <75456280+CristianMihalceanu@users.noreply.github.com>
2021-09-30 01:58:15 +0300

    Add files via upload

---
+++ src/main/java/com/cristi/ImbdSearchApplication.java
@@ -0,0 +1,23 @@
+package com.cristi;
+
+import org.springframework.boot.SpringApplication;
+
+
+import org.json.JSONObject;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImbdSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImbdSearchApplication.class, args);
+	
+	}
+}
